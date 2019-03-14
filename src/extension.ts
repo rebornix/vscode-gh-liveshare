8135f63ce861957fb0d8901055f4074eeb0755ff
Peng Lyu <penn.lv@gmail.com>
2019-03-13 18:07:30 -0700

    remove unnecessary igit

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,11 @@
 'use strict';
 import * as vscode from 'vscode';
 import { API } from '../typings/api';
-import { LiveShareGitProvider } from './vsls';
+import { LiveShareGitManager } from './vsls';
 
 export function activate(context: vscode.ExtensionContext) {
-    const ghpr = vscode.extensions.getExtension('GitHub.vscode-pull-request-github')!.exports;
-    registerLiveShareGitProvider(ghpr);
-}
-
-export function registerLiveShareGitProvider(apiImpl: API): vscode.Disposable {
-	let liveShareGitProvider = new LiveShareGitProvider(apiImpl);
-	return liveShareGitProvider;
+    const api = vscode.extensions.getExtension('GitHub.vscode-pull-request-github')!.exports;
+    context.subscriptions.push(new LiveShareGitManager(api));
 }
 
 // this method is called when your extension is deactivated
